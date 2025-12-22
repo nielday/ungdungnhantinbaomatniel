cebf718b96b1e722d12f0de85f3b822418c6aed1
Phong <phongvipotaku@gmail.com>
2025-12-22 20:19:30 +0700

    Khôi phục logo cũ - bỏ Slack logo
    - Xóa component SlackLogo.tsx
    - Khôi phục BookOpen icon trong Header.tsx với gradient background
    - Khôi phục MessageCircle icon trong ChatApp.tsx với gradient background
    - Trở về logo ban đầu của ứng dụng

--- components/ChatApp.tsx
+++ components/ChatApp.tsx
@@ -25,7 +25,6 @@ import UserSearch from './UserSearch';
 import ProfileModal from './ProfileModal';
 import CreateGroupModal from './CreateGroupModal';
 import GroupManagementModal from './GroupManagementModal';
-import SlackLogo from './SlackLogo';
 
 interface Conversation {
   _id: string;
@@ -334,8 +333,8 @@ export default function ChatApp() {
         <div className="p-4 border-b border-gray-200">
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-3">
-              <div className="w-10 h-10 rounded-full flex items-center justify-center">
-                <SlackLogo size={40} />
+              <div className="w-10 h-10 bg-gradient-to-r from-blue-500 to-purple-600 rounded-full flex items-center justify-center">
+                <MessageCircle className="w-6 h-6 text-white" />
               </div>
               <div>
                 <h1 className="text-lg font-semibold text-gray-800">Tin nhắn</h1>
